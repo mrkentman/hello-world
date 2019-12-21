09c1f1cd0708d2d240b25cacd4b150f574ae80a9
Jordan Kent <jordan@pcjjk.net>
2019-12-21 22:56:09 +0000

    Adding personal best tracker
    Added new personal best tracker

--- test.py
+++ test.py
@@ -1,11 +1,20 @@
 import random
+import time
 
 target = random.randint(1,20)
-print(target) #So I can quickly test the game
 
 i = 1
 guess = 0
-pb = guess
+pb = 0
+
+print("---------------------------------------------------------------------------\n    Hi there my name is Will and I would like to play a game with you.")
+time.sleep(1.5)
+print("\nIn this game I will pick a number from 1 - 20 and you will have to guess \nthe number. Each guess I will tell you whether you are higher or lower\n---------------------------------------------------------------------------\n")
+
+
+def update_pb(n):
+    new_pb = n
+    return new_pb
 
 while i != 0:
     user_guess = int(input("What is your guess: "))
@@ -14,20 +23,19 @@ while i != 0:
         print("\nYou have guessed correctly")
         guess += 1
         print("It took you " + str(guess) + " guesses to get the target number.")
-
         if input("\nWould you like to play again (y/n): ") == "y":
-            if guess != 0 and guess < pb:
-                pb = guess
+            print("\n--------------------------------------\n")
+            if guess != 0 or guess < pb:
+                pb = update_pb(guess)
             guess = 0
             target = random.randint(1,20)
-            print(target)
-
         else:
-            if guess != 0 and guess < pb:
-                pb = guess
-            print("\nThanks for playing.\nYour personal best this session was " + str(pb) + " guesses.")
+            if guess != 0 or guess < pb:
+                pb = update_pb(guess)
+            print("-------------------------------------------------"
+                  "\n              Thanks for playing.              \nYour personal best this session was " + str(pb) + " guesses."
+                  "\n-------------------------------------------------")
             i -= i
-
     elif user_guess > target:
         print("Your guess is higher than the target. Try again\n")
         guess += 1

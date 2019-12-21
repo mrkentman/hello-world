dc570de7075dd2301d34f92b07c1e223414657de
Jordan Kent <jordan@pcjjk.net>
2019-12-21 22:22:51 +0000

    Update test.py

--- test.py
+++ test.py
@@ -3,11 +3,11 @@ import random
 target = random.randint(1,20)
 print(target) #So I can quickly test the game
 
-i = 1
+i = 0
 guess = 0
 pb = guess
 
-while i != 0:
+while i == 0:
     user_guess = int(input("What is your guess: "))
 
     if user_guess == target:
@@ -16,17 +16,13 @@ while i != 0:
         print("It took you " + str(guess) + " guesses to get the target number.")
 
         if input("\nWould you like to play again (y/n): ") == "y":
-            if guess != 0 and guess < pb:
-                pb = guess
             guess = 0
             target = random.randint(1,20)
             print(target)
 
         else:
-            if guess != 0 and guess < pb:
-                pb = guess
             print("\nThanks for playing.\nYour personal best this session was " + str(pb) + " guesses.")
-            i -= i
+            i += 1
 
     elif user_guess > target:
         print("Your guess is higher than the target. Try again\n")

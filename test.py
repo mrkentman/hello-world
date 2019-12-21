10a4c4a12b188d85b01e5cefa1b326740b971a8f
Jordan Kent <jordan@pcjjk.net>
2019-12-21 23:48:51 +0000

    Update test.py

--- test.py
+++ test.py
@@ -12,12 +12,38 @@
     print("\nIn this game I will pick a number from 1 - 20 and you will have to guess \nthe number. Each guess I will tell you whether you are higher or lower\n---------------------------------------------------------------------------\n")
     #Intro text for the game.
 
-print("---------------------------------------------------------------------------\n    Hi there my name is Will and I would like to play a game with you.")
-time.sleep(1.5)
-print("\nIn this game I will pick a number from 1 - 20 and you will have to guess \nthe number. Each guess I will tell you whether you are higher or lower\n---------------------------------------------------------------------------\n")
+    while i != 0:
         user_guess = int(input("What is your guess: ")) #Get the user guess. User guess can either be invalid, correct, above, or below. If expressions to determine what their guess is in relation the the target
 
+        if user_guess > 20 or user_guess < 1: #invalid guess
+            print("This is not a vald guess. Please guess a number between 1 and 20.\n")
             #In case the user makes an invalid guess (i.e. the guess is not between 1 and 20) so that
+            #they can repeat their guess without having it being added to their total.'''
+        elif user_guess == target: #correct guess
+            print("\nYou have guessed correctly")
+            guess += 1
+            print("It took you " + str(guess) + " guess to get the target number.") #Increae guesses by 1 and informs player how many guesses it took them to win.
+            
+            if input("\nWould you like to play again (y/n): ") == "y": #ask the player if they would like to try again
+                print("\n--------------------------------------\n") 
+                if guess != 0 or guess < pb: #updates the players pb if this is their first go (i.e their pb is 0 as they haven't done a game yet) or if that game was lower than the previous pb
+                    pb = guess
+                guess = 0
+                target = random.randint(1,20)
+                print(target)
+            else:
+                if guess != 0 or guess < pb: #updates the players pb if this is their first go (i.e their pb is 0 as they haven't done a game yet) or if that game was lower than the previous pb
+                    pb = guess
+                print("-------------------------------------------------"
+                    "\n              Thanks for playing.              \nYour personal best this session was " + str(pb) + " guesses."
+                    "\n-------------------------------------------------")
+                i -= 1 #ends the loop therfore ending the program/game
+        elif user_guess > target: #guess higher than the target
+            print("Your guess is higher than the target. Try again\n")
+            guess += 1
+        else: #if none of the other options are correct, then the guess is too low
+            print("Your guess is lower than the target. Try again\n")
+            guess += 1
 
     '''To do:
         - Add way for when a '1' is displayed in the guesses it will say the singular version of guess
